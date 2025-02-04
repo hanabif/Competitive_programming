7aa3e7d66c2e0f182071898d8d580f16e82c55eb
Haymi <157227495+hanabif@users.noreply.github.com>
2025-02-04 11:35:47 +0300

    Created 'A - Vus the Cossack and a Contest' by A2SV Hub

---
+++ A2SV G6 - Round #1 04-Feb-2025/A - Vus the Cossack and a Contest 239404.py
@@ -0,0 +1,9 @@
+# Problem: A - Vus the Cossack and a Contest - https://codeforces.com/gym/585107/problem/A
+
+
+n,m,k= map(int,input().split())
+
+if m<n or k<n:
+    print("No")
+else:
+    print("Yes")
