30d9ae7eadac44b3bcc56eeb22b4e99623cf4b70
Haymi <157227495+hanabif@users.noreply.github.com>
2025-02-14 11:27:34 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 14-Feb-2025/C - The Splitting Game 265204.py
@@ -0,0 +1,32 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import defaultdict
+
+n=int(input())
+
+for i in range(n):
+    sum,res=0,0
+    leng=int(input())
+    s=input()
+    left=set()
+    right=defaultdict(int)
+
+    for i in range(leng):
+        right[s[i]]+=1
+    length,maxim=0, len(right)
+   
+    for j in range(len(s)):
+        left.add(s[j])
+        right[s[j]]-=1
+        if right[s[j]]==0:
+            del right[s[j]] 
+        length= len(left)+ len(right)
+        maxim=max(length,maxim)
+    print(maxim)
+
+        
+         
+    
+
+
+  
