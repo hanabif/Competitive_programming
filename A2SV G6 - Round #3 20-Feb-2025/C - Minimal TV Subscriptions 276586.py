582a99f752a039ecf050abc3bea077bdd7cc307f
Haymi <157227495+hanabif@users.noreply.github.com>
2025-02-20 12:59:05 +0300

    Created 'C - Minimal TV Subscriptions' by A2SV Hub

---
+++ A2SV G6 - Round #3 20-Feb-2025/C - Minimal TV Subscriptions 276586.py
@@ -0,0 +1,45 @@
+# Problem: C - Minimal TV Subscriptions - https://codeforces.com/gym/588468/problem/C
+
+#© @haymi
+
+import sys
+from math import ceil, gcd, log2
+from collections import defaultdict, Counter
+ 
+def string(): return sys.stdin.readline().strip()
+def integer(): return int(sys.stdin.readline().strip())
+def integers(): return map(int, sys.stdin.readline().strip().split())
+def List(): return list(map(int, sys.stdin.readline().strip().split()))
+def strings(): return map(str, sys.stdin.readline().strip().split())
+def stringList(): return list(map(str, sys.stdin.readline().strip().split()))
+def Matrix(n): return list(list(map(int, sys.stdin.readline().strip().split())) for i in range(n))
+
+t = integer()
+
+while t:
+    t-=1
+    n , k ,d = integers()
+    shows = List()
+    show_count = defaultdict(int)
+    unique = 0
+    min_unique = float('inf')
+
+    for i in range(d):
+        if show_count[shows[i]] == 0:
+            unique += 1  
+        show_count[shows[i]] += 1
+    min_unique = min(unique, min_unique)
+
+    for i in range(d,n):
+        left = shows[i-d]
+        show_count[left]-=1
+        if show_count[left] == 0:
+            unique-=1
+        
+        right = shows[i]
+        if show_count[right] == 0:
+            unique+=1
+        show_count[right]+=1
+        
+        min_unique = min(min_unique , unique)
+    print(min_unique)
