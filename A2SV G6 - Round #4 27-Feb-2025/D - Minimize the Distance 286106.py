48984fef901d29004502032b08f59e1fce295d29
Haymi <157227495+hanabif@users.noreply.github.com>
2025-02-27 17:32:56 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 27-Feb-2025/D - Minimize the Distance 286106.py
@@ -0,0 +1,13 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+import sys
+
+
+    
+n = int(sys.stdin.readline().strip())
+points = list(map(int, sys.stdin.readline().split()))
+points.sort()
+if n % 2 == 1:
+    print (points[n // 2])
+else:
+    print (points[(n // 2) - 1])
