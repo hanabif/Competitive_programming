00757a981b0809c3ea18e2bd57d2cb7889f30015
Haymi <157227495+hanabif@users.noreply.github.com>
2025-03-11 14:05:19 +0300

    Created 'C - Permutation Minimization' by A2SV Hub

---
+++ A2SV G6 - Round #6 11-Mar-2025/C - Permutation Minimization 302281.py
@@ -0,0 +1,30 @@
+# Problem: C - Permutation Minimization - https://codeforces.com/gym/594077/problem/C
+
+#© @haymi
+
+import sys
+from math import ceil, gcd, log2
+from collections import defaultdict, Counter, deque
+ 
+def string(): return sys.stdin.readline().strip()
+def integer(): return int(sys.stdin.readline().strip())
+def integers(): return map(int, sys.stdin.readline().strip().split())
+def List(): return list(map(int, sys.stdin.readline().strip().split()))
+def strings(): return map(str, sys.stdin.readline().strip().split())
+def stringList(): return list(map(str, sys.stdin.readline().strip().split()))
+def Matrix(n): return list(list(map(int, sys.stdin.readline().strip().split())) for i in range(n))
+
+t = integer()
+while t:
+    t-=1
+    n = integer()
+    nums = List()
+
+    deq =deque([nums[0]])
+    
+    for i in range(1,n):
+        if nums[i] < deq[0]:
+            deq.appendleft(nums[i])
+        else:
+            deq.append(nums[i])
+    print(" ".join(list(map(str,deq))))
