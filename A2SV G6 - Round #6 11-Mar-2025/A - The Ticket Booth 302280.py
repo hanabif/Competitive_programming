8f86daac6da48225011e7fdc25958cfdf9a671d6
Haymi <157227495+hanabif@users.noreply.github.com>
2025-03-11 14:05:12 +0300

    Created 'A - The Ticket Booth' by A2SV Hub

---
+++ A2SV G6 - Round #6 11-Mar-2025/A - The Ticket Booth 302280.py
@@ -0,0 +1,18 @@
+# Problem: A - The Ticket Booth - https://codeforces.com/gym/594077/problem/A
+
+#© @haymi
+
+import sys
+from math import ceil, gcd, log2
+from collections import defaultdict, Counter
+ 
+def string(): return sys.stdin.readline().strip()
+def integer(): return int(sys.stdin.readline().strip())
+def integers(): return map(int, sys.stdin.readline().strip().split())
+def List(): return list(map(int, sys.stdin.readline().strip().split()))
+def strings(): return map(str, sys.stdin.readline().strip().split())
+def stringList(): return list(map(str, sys.stdin.readline().strip().split()))
+def Matrix(n): return list(list(map(int, sys.stdin.readline().strip().split())) for i in range(n))
+
+n , s = integers()
+print(ceil(s/n))
