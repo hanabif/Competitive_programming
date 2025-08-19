015c82d0c9485470ac0b017baab9105d8a3d0018
Haymanot Aweke <157227495+hanabif@users.noreply.github.com>
2025-08-19 18:34:52 +0300

    Created 'Max Consecutive Ones III' by A2SV Hub

---
+++ 19-Aug-2025/Max Consecutive Ones III 386834.py
@@ -0,0 +1,18 @@
+# Problem: Max Consecutive Ones III - https://leetcode.com/problems/max-consecutive-ones-iii/
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        l = 0
+        count = 0
+        ans = 0
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                count += 1
+            
+            while count > k:
+                if nums[l] == 0:
+                    count -= 1
+                l += 1
+            ans = max(ans,r - l + 1)
+        return ans
+                    
