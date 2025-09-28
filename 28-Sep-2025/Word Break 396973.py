09e7160aa85ba2de4578f21638c7be9909696fd6
Haymanot Aweke <157227495+hanabif@users.noreply.github.com>
2025-09-28 11:46:40 +0300

    Created 'Word Break' by A2SV Hub

---
+++ 28-Sep-2025/Word Break 396973.py
@@ -0,0 +1,21 @@
+# Problem: Word Break - https://leetcode.com/problems/word-break/description/
+
+class Solution:
+    def wordBreak(self, s: str, word: List[str]) -> bool:  
+        word = set(word)
+        memo = {}
+        def dp(i):
+            if i == len(s):
+                return True    
+            if i in memo:
+                return memo[i]
+
+            for j in range( i + 1 ,len(s) + 1):
+                if s[i:j] in word and dp(j):
+                    memo[i] = True
+                    return True
+            memo[i] = False
+            return False
+        return dp(0)
+
+ 
