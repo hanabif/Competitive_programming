b3ddf320e13b86b73b61cb0f7cd9b417347adc9c
Haymanot Aweke <157227495+hanabif@users.noreply.github.com>
2025-10-01 11:02:26 +0300

    Created 'A - Frog 1' by A2SV Hub

---
+++ 01-Oct-2025/A - Frog 1 398380.py
@@ -0,0 +1,54 @@
+# Problem: A - Frog 1 - https://atcoder.jp/contests/dp/tasks/dp_a
+
+from functools import reduce
+import sys
+from math import ceil, floor, gcd, log2
+from collections import defaultdict, Counter,deque
+from bisect import bisect_left,bisect_right
+from itertools import accumulate
+
+def string(): return sys.stdin.readline().strip()
+def integer(): return int(sys.stdin.readline().strip())
+def integers(): return map(int, sys.stdin.readline().strip().split())
+def List(): return list(map(int, sys.stdin.readline().strip().split()))
+def strings(): return map(str, sys.stdin.readline().strip().split())
+def stringList(): return list(map(str, sys.stdin.readline().strip().split()))
+def Matrix(n): return list(list(map(int, sys.stdin.readline().strip().split())))
+
+n = integer()
+nums = List()
+memo = {}
+
+from types import GeneratorType
+
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+
+@bootstrap
+def dp(i):
+    if i == 0:
+        yield 0
+    if i == 1:
+        memo[1] = abs(nums[1] - nums[0])
+        yield memo[1]
+    if i not in memo:
+        dp1 = yield dp(i - 1)
+        dp2 = yield dp(i - 2)
+        memo[i] = min(abs(nums[i] - nums[i - 1]) + dp1 , abs(nums[i] - nums[i - 2]) + dp2)
+    yield memo[i]
+print(dp(n - 1))
