b8505df41efc35d85030ef1a9f73fcd79cc6068b
Haymanot Aweke <157227495+hanabif@users.noreply.github.com>
2025-10-11 21:00:06 +0300

    Created 'Complicated GCD' by A2SV Hub

---
+++ 11-Oct-2025/Complicated GCD 402171.py
@@ -0,0 +1,23 @@
+# Problem: Complicated GCD - https://codeforces.com/contest/664/problem/A
+
+from functools import reduce
+import sys
+from math import ceil, floor, gcd, log2
+from collections import defaultdict, Counter,deque
+from bisect import bisect_left,bisect_right
+from itertools import accumulate
+from functools import lru_cache
+
+def string(): return sys.stdin.readline().strip()
+def integer(): return int(sys.stdin.readline().strip())
+def integers(): return map(int, sys.stdin.readline().strip().split())
+def List(): return list(map(int, sys.stdin.readline().strip().split()))
+def strings(): return map(str, sys.stdin.readline().strip().split())
+def stringList(): return list(map(str, sys.stdin.readline().strip().split()))
+def Matrix(n): return list(list(map(int, sys.stdin.readline().strip().split())))
+
+a,b = integers()
+if a != b:
+    print(1)
+else:
+    print(a)
