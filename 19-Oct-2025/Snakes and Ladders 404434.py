4bef808e4ac111bee02a8513c6384ba69c9b4eb0
Haymanot Aweke <157227495+hanabif@users.noreply.github.com>
2025-10-19 18:35:27 +0300

    Created 'Snakes and Ladders' by A2SV Hub

---
+++ 19-Oct-2025/Snakes and Ladders 404434.py
@@ -0,0 +1,31 @@
+# Problem: Snakes and Ladders - https://leetcode.com/problems/snakes-and-ladders/
+
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        length = len(board)
+
+        board.reverse()
+        def pos(square):
+            r = (square - 1) // length
+            c = (square - 1) % length
+            if r % 2:
+                c = length - 1 - c
+            return [r,c]
+
+        q = deque()
+        q.append([1,0])
+        visited = set()
+
+        while q:
+            square, moves = q.popleft()
+            for i in range(1,7):
+                nextSquare = square + i
+                r,c = pos(nextSquare)
+                if board[r][c] != -1:
+                    nextSquare = board[r][c]
+                if nextSquare == length * length:
+                    return moves + 1
+                if nextSquare not in visited:
+                    visited.add(nextSquare)
+                    q.append([nextSquare, moves + 1])
+        return -1
